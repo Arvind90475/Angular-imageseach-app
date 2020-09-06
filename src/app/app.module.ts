8061ac9e1d3c87d870e3a4c1121c5e9f2441c003
arvind <arvind.bisht.90475@gmail.com>
2020-09-06 18:55:15 +1000

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms'
+import { HttpClientModule } from '@angular/common/http'
 
 import { AppComponent } from './app.component';
+import { ImageSearchFormComponent } from './image-search-form/image-search-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ImageSearchFormComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
